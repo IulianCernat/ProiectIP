8f4fe8028a6903448b68d25688c6cfe12fa54968
AnaRosu <ana.maria.rosu.ar@gmail.com>
2019-05-21 15:44:02 +0300

    Integrare front

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,4 @@
 import Model.dao.Problem;
-import Model.dao.User;
-import com.mysql.cj.xdevapi.JsonArray;
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.sql.SQLException;
@@ -10,7 +7,33 @@ import java.sql.SQLException;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
-        
+        Problem pr = new Problem();
+        //System.out.println(pr.getProblem("Numar maxim"));
+
+        //pr.testIfFunctioning();
+        //System.out.println(pr.getEnuntProblema(1));
+        //Document doc    = pr.getTeste(1);
+
+        //File xmlFile = new File("e:\\problem.xml");
+        //pr.addProblem(xmlFile);
+        //System.out.println(pr.getLastProblemId());
+        //System.out.println(pr.getProblemTests(1));
+
+        String message;
+        JSONObject json = new JSONObject();
+
+        json.put("test_in", "Alfabet");
+        json.put("test_out", "ALFABET");
+        json.put("percentage", 10.0 );
+
+        message = json.toString();
+        System.out.println(message);
+        // pr.addTestToProblem(json,3);
+        //System.out.println((pr.getProblemsByGrade(9)).toString());
+        //System.out.println(pr.getTestPercentage(1));
+        JSONObject obj  = pr.getProblem("Numar maxim");
+        System.out.println(obj.getInt("id"));
+
     }
 
 }
