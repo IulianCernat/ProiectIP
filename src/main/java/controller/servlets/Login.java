eb604873036f0d7ca44674c9ac10234cf1fb4d1a
Madalin Mihai <mihai.madalin04@gmail.com>
2019-05-27 14:36:08 +0300

    Adaugat sesiune la login

--- src/main/java/controller/servlets/Login.java
+++ src/main/java/controller/servlets/Login.java
@@ -8,6 +8,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
@@ -26,12 +27,17 @@ public class Login extends HttpServlet {
             String salt = User.getUserSalt(typedUsername);
             String passwordHash = User.md5Hash(typedPassword, salt);
             String  databasePassword = User.getPassword(typedUsername);//.getBytes();
-            System.out.println(passwordHash);
-            System.out.println(databasePassword);
+            //System.out.println(passwordHash);
+            //System.out.println(databasePassword);
 
             if(databasePassword.equals(passwordHash))
             {
                 loginflag = true;
+                HttpSession session = request.getSession(true);
+                //if(session.isNew()) session.setAttribute("username", typedUsername);
+                //else System.out.println("Din sesiune : " + session.getAttribute("username"));
+                session.setAttribute("username", typedUsername); // salvez in sesiune usernameul celui ce s-a conectat
+
                 response.sendRedirect("./html/cont.html");
             }
             else
